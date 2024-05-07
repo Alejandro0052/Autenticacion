f3adaafb97d6f3755afb2809b383bd9cec97bc73
Alejandro <mariaeugenialopez456@mail.com>
2024-05-06 22:40:38 -0500

    Loggin and autentication

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
 using Autenticacion.Model;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace Autenticacion.Pages.Account
 {
@@ -12,9 +14,30 @@ namespace Autenticacion.Pages.Account
         {
         }
 
-        public void OnPost() 
+        public async Task<IActionResult> OnPostAsync() 
         {
-          Console.WriteLine("User: " + User.Email + "Password: " + User.Password);
+         // Console.WriteLine("User: " + User.Email + "Password: " + User.Password);
+         if (!ModelState.IsValid) return Page();
+         
+         if (User.Email == "correo@gmail.com" && User.Password == "12345")
+         {
+                //creando los claim, datos a almacenar en la cookie
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email,User.Email),
+                };
+               //asociando los claims creados al nombre de una cookie
+                var identity = new ClaimsIdentity(claims,"MyCookieAuth");
+                //se agrega la identidad crada al ClaimPrincipal de la apliacacion
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                //registro exitoso y se crea la cookie en el navegador
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+                return RedirectToPage("/index");
+         }
+            return Page();  
+      
         }
     }
 
